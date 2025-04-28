d1c5bebf8735a7a6f657a1f5f9a7662d9eccf488
miq123dad <miq123dad@gmail.com>
2025-04-28 18:24:42 +0300

    initial

---
+++ android/app/src/main/kotlin/com/orders/ordersmanagment_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.orders.ordersmanagment_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'features/auth/presentation/screens/login_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Sales App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: LoginScreen(),
+    );
+  }
+}
